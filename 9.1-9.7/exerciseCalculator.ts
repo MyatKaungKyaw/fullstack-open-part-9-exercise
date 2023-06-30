af0c90113795f975fb1162baeca4021c575bfd04
Myat Kaung Kyaw <33856024+MyatKaungKyaw@users.noreply.github.com>
2023-06-30 12:25:02 +0630

    develop calculateExercises function

--- 9.1-9.7/exerciseCalculator.ts
+++ 9.1-9.7/exerciseCalculator.ts
@@ -14,22 +14,33 @@ interface rate{
 const calculateExercises = (dailyHours: [number], targetHour: number): exercisesResult => {
     const trainingDays = dailyHours.filter(hour => hour > 0).length;
     const periodLength = dailyHours.length;
-    const rate = ((): rate => {
+    const rateResult = ((): rate => {
         const percent = trainingDays / periodLength
         if (percent >= 0.7) {
-            return 3;
+            return {
+                rating:3, 
+                ratingDescription: 'good',
+            };
         } else if (percent >= 0.5) {
-            return 2;
+            return {
+                rating: 2,
+                ratingDescription: 'not bad',
+            };
         }
-        return 1;
+        return {
+            rating: 1,
+            ratingDescription: 'not bad',
+        };
     })();
+    const average = dailyHours.reduce((total,hour) => total += hour,0)/dailyHours.length;
 
     return {
         periodLength: periodLength,
         trainingDays: trainingDays,
         success: trainingDays >= periodLength,
-        rating: rate.rating,
-        ratingDescription: rate.ratingDescription,
-        
+        rating: rateResult.rating,
+        ratingDescription: rateResult.ratingDescription,
+        target: targetHour,
+        average: average,
     };
 };
