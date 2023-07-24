b9bed5212e5794c2fe80104a00966cf725d5c3c1
Myat Kaung Kyaw <33856024+MyatKaungKyaw@users.noreply.github.com>
2023-07-24 09:08:34 +0000

    implement cors and change port to 3001

--- patientor_backend/index.ts
+++ patientor_backend/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/ping',(_req,res)=>{
     return res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT,() => {
     console.log(`Server is listening on ${PORT}`);
