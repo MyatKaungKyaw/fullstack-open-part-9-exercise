930b71c1d2cbf9bc8a21de60ed7fba6d31ca2e0e
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-08-03 21:22:04 +0630

    use type in part

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,20 +12,93 @@ const Content = (props: CoursePartsProps) => (
   <>
     {props.courseParts.map((coursePart) => (
       <p key={coursePart.name}>
-        {coursePart.name} {coursePart.exerciseCount}
+        <Part coursePart={coursePart} />
       </p>
     ))}
   </>
 );
 
-interface CoursePart {
+const Part = ({ coursePart }: coursePartProp) => {
+  const title = <h2>{coursePart.name} {coursePart.exerciseCount}</h2>;
+  switch (coursePart.kind) {
+    case 'group':
+      return <>
+        {title}
+        <p>project exercises {coursePart.groupProjectCount}</p>
+      </>;
+    case 'background':
+      return <>
+        {title}
+        {coursePart.description}<br />
+        submit to {coursePart.backgroundMaterial}
+      </>;
+    case 'basic':
+      return <>
+        {title}
+        <p>{coursePart.description}</p>
+      </>
+    case 'special':
+      return <>
+        {title}
+        {coursePart.description}<br />
+        required skills: {coursePart.requirements
+          .map((req, index) =>
+            coursePart.requirements.length === index + 1
+              ? req
+              : req + ','
+          )};
+      </>
+    default:
+      return coursePart;
+  }
+};
+
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+interface CoursePartSpeical extends CoursePartDescription {
+  requirements: string[];
+  kind: 'special'
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpeical;
+
 interface CoursePartsProps {
   courseParts: CoursePart[];
 }
+
+interface coursePartProp {
+  coursePart: CoursePart;
+}
+
+/**
+ * Helper function for exhaustive type checking
+ */
+// const assertNever = (value: never): never => {
+//   throw new Error(
+//     `Unhandled discriminated union member: ${JSON.stringify(value)}`
+//   );
+// };
 
 const Total = (props: CoursePartsProps) => (
   <>
@@ -36,26 +109,53 @@ const Total = (props: CoursePartsProps) => (
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     },
   ];
 
   return (
     <div>
-      <Header header={courseName}/>
-      <Content courseParts={courseParts}/>
-      <Total courseParts={courseParts}/>
+      <Header header={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
     </div>
   );
 };
