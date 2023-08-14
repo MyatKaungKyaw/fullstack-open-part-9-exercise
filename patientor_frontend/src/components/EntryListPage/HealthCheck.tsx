206c957c15e142fc4051a9da38563fcc2e57dbb7
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-08-14 19:17:14 +0900

    use icon to show health check rate

--- patientor_frontend/src/components/EntryListPage/HealthCheck.tsx
+++ patientor_frontend/src/components/EntryListPage/HealthCheck.tsx
@@ -1,13 +1,31 @@
-import { Typography } from "@mui/material"
-import { HealthCheckEntry } from "../../types"
+import { HealthCheckEntry, HealthCheckRating } from "../../types"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { deepOrange, yellow } from "@mui/material/colors";
 
 interface Props {
     entry: HealthCheckEntry;
 }
 
 const HealthCheck = ({ entry }: Props) => {
+    const HCRValues: number[] = Object.values(HealthCheckRating).map(h => Number(h)).filter(h => !isNaN(h));
+    const maxHCR = Math.max(...HCRValues);
+    const minHCR = Math.min(...HCRValues);
+    const midHCR = (maxHCR-minHCR)/2;
+
+    const HCRcolor = (hcr: HealthCheckRating)=>{
+        if(hcr===minHCR){
+            return {color: yellow[500]}
+        }else if(hcr < midHCR){
+            return {color:yellow[800]}
+        }else if(hcr === midHCR){
+            return {color:deepOrange[500]}
+        }else{
+            return {color:deepOrange[800]}
+        }
+    }
+    
     return (<>
-        <Typography variant='body1'>health check rating: {entry.healthCheckRating}</Typography>
+        <FavoriteIcon sx={HCRcolor(entry.healthCheckRating)}/>
     </>)
 }
 
